6669f4849398b23bd3fc208f323d1486f98cc2d9
mz <mz@thehouse.com>
2023-09-23 13:19:18 +0200

    Done: 09/23/23 13:19:18

--- checkers.py
+++ checkers.py
@@ -9,8 +9,7 @@ import pygame
 from checkerboard.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED, WHITE
 from checkerboard.board import Board
 from checkerboard.game import Game
-from minimax import Minimax
-
+import minimax
 import parser
 
 results, moves = parser.run()
@@ -31,43 +30,59 @@ def get_row_col_from_mouse(pos):        # Aus der Position der Maus wird berechn
     col = x // SQUARE_SIZE
     return row, col
 
-
-def main():
+def run():
     run = True
-    clock = pygame.time.Clock()             # Das ermöglicht, dass die Zeit für einen Spielzug vom Computer reguliert ist
+    clock = pygame.time.Clock()
     game = Game(WIN)
-    minimax = Minimax()
-
     while run:
         clock.tick(FPS)
-        for event in pygame.event.get():    #Diese For-Schleife wird gemacht um auszuwählen, ob man aufhören will zu spielen oder etwas anderes machen will
-            if event.type == pygame.QUIT:
-                run = False                 #Falls man den Quit button drückt wird die ganze while schleife gestoppt und das Spiel endet
-
-            if event.type == pygame.MOUSEBUTTONDOWN:    #Falls man seine Maus klickt wird hier alles definiert zur Spielzugausführung
-                pos = pygame.mouse.get_pos()
-
-                # game.select(row, col)
-
-                if game.turn == RED:
-                  # row, col = get_row_col_from_mouse(pos)  #Wenn wir also die Maus auf einem Feld klicken, dann wird es uns diesen Code ausführen und wissen welche Spielfigur, wir gedrückt haben
-                  # print(f'Selected: (row, col) = ({row}, {col})')
-                  # print(f'Game piece = {game.board.get_piece(row, col)}')
-                  # game.select(row, col)
-                  (pr, pc), (row, col) = minimax.move(game.board, game.turn)
-                  print(f'Minmax Selected: (pr, pc, row, col) = ({pr}, {pc}, {row}, {col})')
-                  game.select(pr, pc)
-                  game.select(row, col)
-                else:
-                  (pr, pc), (row, col) = minimax.move(game.board, game.turn)
-                  print(f'Minmax Selected: (pr, pc, row, col) = ({pr}, {pc}, {row}, {col})')
-                  game.select(pr, pc)
-                  game.select(row, col)
-
+        (pr, pc), (row, col) = minimax.move(game.board, game.turn)
+        print(f'Minmax Selected: (pr, pc, row, col) = ({pr}, {pc}, {row}, {col})')
+        game.select(pr, pc)
+        game.select(row, col)
+        time.sleep(0.1)
         game.update()
-
     pygame.quit()
     print(game.winner())
+
+
+def main():
+  run()
+
+    # run = True
+    # clock = pygame.time.Clock()             # Das ermöglicht, dass die Zeit für einen Spielzug vom Computer reguliert ist
+    # game = Game(WIN)
+
+    # while run:
+    #     clock.tick(FPS)
+    #     for event in pygame.event.get():    #Diese For-Schleife wird gemacht um auszuwählen, ob man aufhören will zu spielen oder etwas anderes machen will
+    #         if event.type == pygame.QUIT:
+    #             run = False                 #Falls man den Quit button drückt wird die ganze while schleife gestoppt und das Spiel endet
+
+    #         if event.type == pygame.MOUSEBUTTONDOWN:    #Falls man seine Maus klickt wird hier alles definiert zur Spielzugausführung
+    #             pos = pygame.mouse.get_pos()
+
+    #             # game.select(row, col)
+
+    #             if game.turn == RED:
+    #               # row, col = get_row_col_from_mouse(pos)  #Wenn wir also die Maus auf einem Feld klicken, dann wird es uns diesen Code ausführen und wissen welche Spielfigur, wir gedrückt haben
+    #               # print(f'Selected: (row, col) = ({row}, {col})')
+    #               # print(f'Game piece = {game.board.get_piece(row, col)}')
+    #               # game.select(row, col)
+    #               (pr, pc), (row, col) = minimax.move(game.board, game.turn)
+    #               print(f'Minmax Selected: (pr, pc, row, col) = ({pr}, {pc}, {row}, {col})')
+    #               game.select(pr, pc)
+    #               game.select(row, col)
+    #             else:
+    #               (pr, pc), (row, col) = minimax.move(game.board, game.turn)
+    #               print(f'Minmax Selected: (pr, pc, row, col) = ({pr}, {pc}, {row}, {col})')
+    #               game.select(pr, pc)
+    #               game.select(row, col)
+
+    #     game.update()
+
+    # pygame.quit()
+    # print(game.winner())
 
 
 main()
--- minimax.py
+++ minimax.py
@@ -1,33 +1,30 @@
 #Minimax-algorhythmus mit Bewertungfunktion
 
-import pygame
 import random
-from checkerboard.constants import ROWS, COLS, RED, WHITE
-from checkerboard.board import Board
+from checkerboard.constants import ROWS, COLS
 
-class Minimax:
 
-    def get_all_valid_moves(self, board, turn):
-      all_valid_moves = []
-      amount_piece_moves = 0
-      pieces = board.get_all_pieces(turn)
-      print('pieces:', pieces)
+def _get_all_valid_moves(board, turn):
+  all_valid_moves = []
+  amount_piece_moves = 0
+  pieces = board.get_all_pieces(turn)
+  print('pieces:', pieces)
 
-      for row in range(ROWS):
-        for col in range(COLS):
-          piece = board.get_piece(row, col)
-          if piece == 0 or piece.color != turn:
-            continue
-          # Now check only pieces whose turn is to move.
-          valid_moves = board.get_valid_moves(piece)
-          print('piece:', piece.color, piece.row, piece.col, valid_moves)
-          for (mr, mc) in valid_moves.keys():
-            all_valid_moves.append(((piece.row, piece.col), (mr, mc)))
-            amount_piece_moves += 1
-      print('There are', amount_piece_moves, 'pieces that can move')
-      return all_valid_moves
+  for row in range(ROWS):
+    for col in range(COLS):
+      piece = board.get_piece(row, col)
+      if piece == 0 or piece.color != turn:
+        continue
+      # Now check only pieces whose turn is to move.
+      valid_moves = board.get_valid_moves(piece)
+      print('piece:', piece.color, piece.row, piece.col, valid_moves)
+      for (mr, mc) in valid_moves.keys():
+        all_valid_moves.append(((piece.row, piece.col), (mr, mc)))
+        amount_piece_moves += 1
+  print('There are', amount_piece_moves, 'pieces that can move')
+  return all_valid_moves
 
-    def move(self, board, turn):
-      valid_moves = self.get_all_valid_moves(board, turn)
-      print('Valid Moves:', valid_moves)
-      return random.choice(valid_moves)
+def move(board, turn):
+  valid_moves = _get_all_valid_moves(board, turn)
+  print('Valid Moves:', valid_moves)
+  return random.choice(valid_moves)
