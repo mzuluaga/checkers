5d6af7dc8b9387660eb5fcf49877eb77cb65bbb9
mz <mz@thehouse.com>
2024-01-04 17:22:41 +0100

    Done: 01/04/24 17:22:41

--- minimax.py
+++ minimax.py
@@ -3,7 +3,7 @@ from checkerboard.constants import RED, WHITE
 import state
 
 INF=1e6
-DEPTH=3
+DEPTH=5   # will have DEPTH+1 levels. (has to be odd).
 
 def end_game(board, turn):
   for m in state.get_moves(board, turn):
@@ -27,23 +27,27 @@ def max_strength(board, alpha, beta, depth):
     if _s < tmp_s:
       _s, _move, _board = tmp_s, m, new_board
     if _s >= beta:
+      print(f'out in max function beta test: {_s} >= {beta}')
       break
-    alph = max(alpha, _s)
+    alpha = max(alpha, _s)
+    #print(f'alpha = {alpha} {_s} {depth}')
   return _s, _move, _board
 
 
 def min_strength(board, alpha, beta, depth):
   turn = RED
   if depth == 0 or end_game(board, turn):
-    return state.evaluate(board, turn), None, None
+    return state.evaluate(board, WHITE), None, None
   _s, _move, _board = INF, None, None
   for m, new_board in succesors(board, turn):
     tmp_s, _, _ = max_strength(new_board, alpha, beta, depth-1)
     if _s > tmp_s:
       _s, _move, _board = tmp_s, m, new_board
     if _s <= alpha:
+      print(f'out in min function alha test: {_s} <= {alpha}')
       break
     beta = min(beta, _s)
+    print(f'beta = {beta} {_s} {depth}')
   return _s, _move, _board
 
 
--- state_test.py
+++ state_test.py
@@ -5,7 +5,7 @@ class StateTest(unittest.TestCase):
 
   def test_moves_without_jump(self):
     board = state.new_board()
-    state.print_board(board)
+    #state.print_board(board)
     m = ((5, 2), (4, 3))
     board = state.move(board, m)
     self.assertEqual(board[5,2], 0)
@@ -26,11 +26,30 @@ class StateTest(unittest.TestCase):
     board[3, 4] = state.W
     board[4, 3] = state.R
     m = ((4, 3), (2, 5))
-    state.print_board(board)
+    #state.print_board(board)
     board = state.move(board, m)
-    state.print_board(board)
+    #state.print_board(board)
     self.assertEqual(board.sum(), 1)
 
+  def test_moves_white_jumps_over_red_queen(self):
+    board = state.empty_board()
+    board[1, 0] = state.W
+    board[2, 1] = state.RK
+    #state.print_board(board)
+    m = ((1, 0), (3, 2))
+    board = state.move(board, m)
+    #state.print_board(board)
+    self.assertEqual(board.sum(), 2)
+
+  def test_captures_and_promotes_to_red_queen(self):
+    board = state.empty_board()
+    board[1, 2] = state.W
+    board[2, 1] = state.R
+    #state.print_board(board)
+    m = ((2, 1), (0, 3))
+    board = state.move(board, m)
+    #state.print_board(board)
+    self.assertEqual(board.sum(), 3)
 
 if __name__ == '__main__':
     unittest.main()
