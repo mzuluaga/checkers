65f51fd85c6f6d1e68bec03e439ce0227a0406b9
mz <mz@thehouse.com>
2024-01-05 12:08:13 +0100

    Done: 01/05/24 12:08:13

--- minimax.py
+++ minimax.py
@@ -6,14 +6,7 @@ import state
 INF=1e6
 DEPTH=3   # will have DEPTH+1 levels. (has to be odd).
 
-def utility(board):
-  if state.count_red(board) == 0:
-    return INF
-  if state.count_white(board) == 0:
-    return -INF
-  return state.evaluate(board, WHITE)
-
-# # simple utility from white's perspective.
+# # simpler utility heuristic for testing.
 # # large positive numbers mean they are better for white.
 # def utility(board):
 #   if state.count_red(board) == 0:
@@ -23,6 +16,14 @@ def utility(board):
 #   rc = state.count_red(board)
 #   wc = state.count_white(board)
 #   return (100 / (rc + 1)) + (-90 / (wc + 1))
+
+
+def utility(board):
+  if state.count_red(board) == 0:
+    return INF
+  if state.count_white(board) == 0:
+    return -INF
+  return state.evaluate(board, WHITE)
 
 
 def successors(board, turn):
@@ -45,7 +46,7 @@ def max_strength(board, alpha, beta, depth):
     alpha = max(alpha, _s)
     #print(f'alpha = {alpha} {_s} {depth}')
     if _s >= beta:
-      print(f'out in max function beta test: {_s} >= {beta}')
+      #print(f'out in max function beta test: {_s} >= {beta}')
       break
   assert _move is not None
   return _s, _move, _board
@@ -63,7 +64,7 @@ def min_strength(board, alpha, beta, depth):
     beta = min(beta, _s)
     #print(f'beta = {beta} {_s} {depth}')
     if _s <= alpha:
-      print(f'out in min function alpha test: {_s} <= {alpha}')
+      #print(f'out in min function alpha test: {_s} <= {alpha}')
       break
   return _s, _move, _board
 
