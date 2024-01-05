c71fa68e01f55eb005861e1da67295cbe9040850
mz <mz@thehouse.com>
2024-01-05 09:03:58 +0100

    Done: 01/05/24 09:03:58

--- minimax_test.py
+++ minimax_test.py
@@ -5,8 +5,10 @@ import state
 import minimax
 from checkerboard.constants import RED, WHITE
 
-WK = state.WK
 R = state.R
+RK = state.RK
+W = state.W
+WK = state.WK
 
 class MinimaxTest(unittest.TestCase):
 
@@ -46,17 +48,29 @@ class MinimaxTest(unittest.TestCase):
   # 5 □ □ □ □ □ □ □ □
   # 6 □ □ □ □ □ □ □ □
   # 7 □ □ □ □ □ □ □ □
-  def test_successors_works(self):
+  # def test_successors_for_red(self):
+  #   board = np.zeros((8,8), np.int8)
+  #   board[4] = [0, 0, 0, WK, 0, 0, 0, 0]
+  #   board[5] = [0, 0, 0, 0,  R, 0, 0, 0]
+  #   print('original board')
+  #   state.print_board(board)
+  #   successors = list(minimax.successors(board, RED))
+  #   self.assertEqual(len(successors), 2)
+  #   self.assertEqual(successors[0][1].sum(), 1)
+  #   self.assertEqual(successors[1][1].sum(), 5)
+
+  def test_successors_for_white(self):
     board = np.zeros((8,8), np.int8)
-    board[4] = [0, 0, 0, WK, 0, 0, 0, 0]
-    board[5] = [0, 0, 0, 0,  R, 0, 0, 0]
+    board[4] = [0, 0, 0, W, 0, 0, 0, 0]
+    board[5] = [0, 0, 0, 0,  RK, 0, 0, 0]
     print('original board')
     state.print_board(board)
-    successors = list(minimax.successors(board, RED))
+    successors = list(minimax.successors(board, WHITE))
+    for m, mboard in successors:
+      state.print_board(mboard)
     self.assertEqual(len(successors), 2)
-    self.assertEqual(successors[0][1].sum(), 1)
-    self.assertEqual(successors[1][1].sum(), 5)
-
+    # self.assertEqual(successors[0][1].sum(), 1)
+    # self.assertEqual(successors[1][1].sum(), 5)
 
 if __name__ == '__main__':
     unittest.main()
