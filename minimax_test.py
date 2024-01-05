8e7ae88253139691af842db3bc4a9d7fad6564b5
mz <mz@thehouse.com>
2024-01-05 11:02:10 +0100

    Done: 01/05/24 11:02:10

--- minimax_test.py
+++ minimax_test.py
@@ -12,33 +12,48 @@ WK = state.WK
 
 class MinimaxTest(unittest.TestCase):
 
-  def test_successors_for_red(self):
-    board = np.zeros((8,8), np.int8)
-    board[4] = [0, 0, 0, WK, 0, 0, 0, 0]
-    board[5] = [0, 0, 0, 0,  R, 0, 0, 0]
-    print('original board (RED/BLACK plays):')
-    state.print_board(board)
-    successors = list(minimax.successors(board, RED))
-    for m, mboard in successors:
-      state.print_board(mboard)
-      print(f'Utility from WHITE perspective: {minimax.utility(mboard)}')
-    self.assertEqual(len(successors), 2)
-    self.assertEqual(successors[0][1].sum(), 1)
-    self.assertEqual(successors[1][1].sum(), 5)
+  # def test_successors_for_red(self):
+  #   board = np.zeros((8,8), np.int8)
+  #   board[4] = [0, 0, 0, WK, 0, 0, 0, 0]
+  #   board[5] = [0, 0, 0, 0,  R, 0, 0, 0]
+  #   print('original board (RED/BLACK plays):')
+  #   state.print_board(board)
+  #   successors = list(minimax.successors(board, RED))
+  #   for m, mboard in successors:
+  #     state.print_board(mboard)
+  #     print(f'Utility from WHITE perspective: {minimax.utility(mboard)}')
+  #   self.assertEqual(len(successors), 2)
+  #   self.assertEqual(successors[0][1].sum(), 1)
+  #   self.assertEqual(successors[1][1].sum(), 5)
+
+  # def test_successors_for_white(self):
+  #   board = np.zeros((8,8), np.int8)
+  #   board[4] = [0, 0, 0, W, 0, 0, 0, 0]
+  #   board[5] = [0, 0, 0, 0,  RK, 0, 0, 0]
+  #   print('original board (WHITE plays):')
+  #   state.print_board(board)
+  #   successors = list(minimax.successors(board, WHITE))
+  #   for m, mboard in successors:
+  #     state.print_board(mboard)
+  #     print(f'Utility from WHITE perspective: {minimax.utility(mboard)}')
+  #   self.assertEqual(len(successors), 2)
+  #   self.assertEqual(successors[0][1].sum(), 5)
+  #   self.assertEqual(successors[1][1].sum(), 2)
 
   def test_successors_for_white(self):
     board = np.zeros((8,8), np.int8)
-    board[4] = [0, 0, 0, W, 0, 0, 0, 0]
-    board[5] = [0, 0, 0, 0,  RK, 0, 0, 0]
+    board[0] = [0, W, 0, 0, 0, 0, 0, 0]
+    board[1] = [0, 0, R, 0, 0, 0, 0, 0]
     print('original board (WHITE plays):')
     state.print_board(board)
     successors = list(minimax.successors(board, WHITE))
     for m, mboard in successors:
       state.print_board(mboard)
-      print(f'Utility from WHITE perspective: {minimax.utility(mboard)}')
+      print(f'Utility from WHITE perspective {m}: {minimax.utility(mboard)}')
     self.assertEqual(len(successors), 2)
     self.assertEqual(successors[0][1].sum(), 5)
     self.assertEqual(successors[1][1].sum(), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
