f85e6c7b4783da5ab796216c7a3aabf8de9b2281
mz <mz@thehouse.com>
2024-01-05 10:14:53 +0100

    Done: 01/05/24 10:14:53

--- minimax_test.py
+++ minimax_test.py
@@ -12,52 +12,18 @@ WK = state.WK
 
 class MinimaxTest(unittest.TestCase):
 
-  #   Board:
-  #   0 1 2 3 4 5 6 7
-  # 0 □ □ □ □ □ □ □ □
-  # 1 □ □ □ □ □ □ □ □
-  # 2 □ □ □ □ □ □ □ □
-  # 3 □ □ □ □ □ □ □ □
-  # 4 □ □ □ ♛ □ □ □ □
-  # 5 □ □ □ □ ♙ □ □ □
-  # 6 □ □ □ □ □ □ □ □
-  # 7 □ □ □ □ □ □ □ □
-  #
-  # candidate 0 ((5, 4), (3, 2))
-  #
-  # Board:
-  #   0 1 2 3 4 5 6 7
-  # 0 □ □ □ □ □ □ □ □
-  # 1 □ □ □ □ □ □ □ □
-  # 2 □ □ □ □ □ □ □ □
-  # 3 □ □ ♙ □ □ □ □ □
-  # 4 □ □ □ □ □ □ □ □
-  # 5 □ □ □ □ □ □ □ □
-  # 6 □ □ □ □ □ □ □ □
-  # 7 □ □ □ □ □ □ □ □
-  #
-  # candidate 1 ((5, 4), (4, 5))
-  #
-  # Board:
-  #   0 1 2 3 4 5 6 7
-  # 0 □ □ □ □ □ □ □ □
-  # 1 □ □ □ □ □ □ □ □
-  # 2 □ □ □ □ □ □ □ □
-  # 3 □ □ □ □ □ □ □ □
-  # 4 □ □ □ ♛ □ ♙ □ □
-  # 5 □ □ □ □ □ □ □ □
-  # 6 □ □ □ □ □ □ □ □
-  # 7 □ □ □ □ □ □ □ □
-  # def test_successors_for_red(self):
-  #   board = np.zeros((8,8), np.int8)
-  #   board[4] = [0, 0, 0, WK, 0, 0, 0, 0]
-  #   board[5] = [0, 0, 0, 0,  R, 0, 0, 0]
-  #   print('original board')
-  #   state.print_board(board)
-  #   successors = list(minimax.successors(board, RED))
-  #   self.assertEqual(len(successors), 2)
-  #   self.assertEqual(successors[0][1].sum(), 1)
-  #   self.assertEqual(successors[1][1].sum(), 5)
+  def test_successors_for_red(self):
+    board = np.zeros((8,8), np.int8)
+    board[4] = [0, 0, 0, WK, 0, 0, 0, 0]
+    board[5] = [0, 0, 0, 0,  R, 0, 0, 0]
+    print('original board')
+    state.print_board(board)
+    successors = list(minimax.successors(board, RED))
+    for m, mboard in successors:
+      state.print_board(mboard)
+    self.assertEqual(len(successors), 2)
+    self.assertEqual(successors[0][1].sum(), 1)
+    self.assertEqual(successors[1][1].sum(), 5)
 
   def test_successors_for_white(self):
     board = np.zeros((8,8), np.int8)
@@ -69,8 +35,8 @@ class MinimaxTest(unittest.TestCase):
     for m, mboard in successors:
       state.print_board(mboard)
     self.assertEqual(len(successors), 2)
-    # self.assertEqual(successors[0][1].sum(), 1)
-    # self.assertEqual(successors[1][1].sum(), 5)
+    self.assertEqual(successors[0][1].sum(), 5)
+    self.assertEqual(successors[1][1].sum(), 2)
 
 if __name__ == '__main__':
     unittest.main()
